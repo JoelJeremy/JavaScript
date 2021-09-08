fe2fb1b8163157a1b027260fbac9548572b14d11
JoelJeremy <amahj@roc-dev.com>
2021-09-08 19:35:58 +0200

    opdracht2.2

---
+++ opdracht2/opdracht2.2.js
@@ -0,0 +1,6 @@
+bl1.style.backgroundColor = "aqua";
+bl2.style.backgroundColor = "green";
+bl3.style.backgroundColor = "yellow";
+bl4.style.backgroundColor = "red";
+
+/*-- achtergond kleuren veranderd --*/
