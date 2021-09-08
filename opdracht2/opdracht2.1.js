44c2a2dc3da5103daea5404b565c66caba2c72dc
JoelJeremy <amahj@roc-dev.com>
2021-09-08 15:26:21 +0200

    opdracht2.1

---
+++ opdracht2/opdracht2.1.js
@@ -0,0 +1,11 @@
+
+
+/*-- pagina titel --*/
+document.getElementById("paginatitel").innerText = "Dit is de pagina titel";
+/*-- Subkop --*/
+document.getElementById("subkop").innerText = "Dit is de subkop";
+/*-- adidas human race yellow --*/
+document.getElementById("humanrace").src="javaimages/humanraceyellow.jfif";
+/*-- black yeezy --*/
+document.getElementById("yeezyblack").src="javaimages/black.jfif";
+
