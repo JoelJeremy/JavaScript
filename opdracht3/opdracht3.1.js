799a38893b03f1e1a993598427bcb8b37db157c6
JoelJeremy <amahj@roc-dev.com>
2021-09-14 09:44:57 +0200

    opdracht3

---
+++ opdracht3/opdracht3.1.js
@@ -0,0 +1,18 @@
+var plaatjes = document.getElementsByTagName("img");
+var random=0;
+var randomnummers = [];
+
+while(randomnummers.length < 9) {
+    random = Math.floor(Math.random() *9) +1;
+    if (randomnummers.lastIndexOf(random) == -1) {
+        randomnummers.push(random);
+    }
+}
+
+random = 0;
+for (var plaatje in plaatjes) {
+    plaatjes[plaatje].src="javaimage3/auto"+ randomnummers[random] + ".jpg";
+    random++;
+}
+
+
